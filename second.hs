1e2a96798b07c095f627d165fc6219179bef8bd1
cupertank <ilyavologin@gmail.com>
2020-06-05 16:32:57 +0300

    Second task

---
+++ second.hs
@@ -0,0 +1,26 @@
+import Control.Monad
+
+data BSTree a = Nil 
+              | Node {val :: a, left :: BSTree a, right :: BSTree a}
+                deriving Show
+
+instance Foldable BSTree where
+    foldr f acc Nil                   = acc
+    foldr f acc (Node val left right) = do
+        foldr f (f val acc) right
+        f val acc
+
+
+findElem tree = foldM findElem' (Nothing, Nothing) tree
+
+findElem' (Nothing, Nothing) val = return (Nothing, Just val)
+findElem' (Nothing, Just right)   val = return (Just val, Just right)
+
+findElem' (Just parent, Just praparent) val = do
+    if parent * praparent == val then
+        print val
+    else
+        print ""
+    return (Just val, Just parent)
+
+tree =  Node 5 (Node 2 Nil (Node 10 Nil Nil)) Nil
